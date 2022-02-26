19d4d77663779d2d02ee522e9542983411411b66
Shana <shana.lavina.moore@gmail.com>
2022-02-26 05:27:17 -0800

    add tips to guide test development

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,9 +3,18 @@ require './lib/calculator.rb'
 describe Calculator do
   describe "#add" do
     it "adds two numbers" do
+      # FOUR PHASES OF TESTING:
+      # 1. Setup - get the conditions correct for the test
       calculator = Calculator.new
 
-      expect(calculator.add(1, 1)).to eq(2)
+      # 2. Exercise - perform the thing that you're testing
+      sum = calculator.add(5, 10) 
+
+      # 3. Verification - verify that the exercise did what you expected
+      expect(sum).to eq(15)
+
+      # 4. Teardown - undo any conditions that shouldn't persist post-test
+      # (In this test the teardown phase isn't necessary.)
     end
   end
 end
