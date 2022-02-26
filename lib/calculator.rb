1d3021010dee8c251b9b13f9f460db126297e5e6
Shana <shana.lavina.moore@gmail.com>
2022-02-26 10:28:37 -0800

    update instruction comment

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,5 @@
-# Use PPP principles to build out the following functionality for a Calculator. 
-# Here are the requirements:
+# Use Ping-Pong Programming (PPP) principles to build out the following functionality for a Calculator. 
+# Here are the required methods:
 # add
 # subtract
 # multiply
@@ -9,11 +9,11 @@
 # square root
 # factorial
 
-# We've already started to implement the add spec in demo.
+# To get you started, we've already started to implement the spec for #add per the PPP Workshop demo.
 # With your PPP partner(s), start by running the specs, watch them fail, then 
-# write the code to make them pass. Then move onto implementing the other
-# methods (listed above) per the PPP principles and don't forget to think about
-# any edge cases. (HINT for division - what should the user expect when they divide by zero?)
+# write the code to make them pass, incrementally. 
+# Then move onto implementing the other specs and code for the methods listed above, per the PPP principles.
+# Don't forget to think about any edge cases. (HINT for division - what should the user expect when they divide by zero?)
 # slide deck: https://docs.google.com/presentation/d/17tLRoC7_LP1q9hSF-4B0g3nKrTQXKo3qUI3VlIMaxC8/edit?usp=sharing
  
 # class Calculator
