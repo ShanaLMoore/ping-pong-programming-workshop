8240827b975cb918eb9441a4b2c48f0874e01b0d
Shana <shana.lavina.moore@gmail.com>
2022-02-25 21:44:08 -0800

    first commit

---
+++ lib/calculator.rb
@@ -0,0 +1,23 @@
+# Use PPP principles to build out the following functionality for a Calculator. 
+# Here are the requirements:
+# add
+# subtract
+# multiply
+# dividie
+# modulo
+# square
+# square root
+# factorial
+
+# We've already started to implement the add spec in demo.
+# With your PPP partner(s), start by running the specs, watch them fail, then 
+# write the code to make them pass. Then move onto implementing the other
+# methods (listed above) per the PPP principles and don't forget to think about
+# any edge cases. (HINT for division - what should the user expect when they divide by zero?)
+# slide deck: https://docs.google.com/presentation/d/17tLRoC7_LP1q9hSF-4B0g3nKrTQXKo3qUI3VlIMaxC8/edit?usp=sharing
+ 
+# class Calculator
+#   def add(a, b)
+
+#   end
+# end
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,11 @@
+require './lib/calculator.rb'
+
+describe Calculator do
+  describe "#add" do
+    it "adds two numbers" do
+      calculator = Calculator.new
+
+      expect(calculator.add(1, 1)).to eq(2)
+    end
+  end
+end
