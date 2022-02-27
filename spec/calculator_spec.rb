b7740aede05027f52f34c1dfb42055898fa56bd1
Shana Moore <shana.lavina.moore@gmail.com>
2022-02-27 07:38:07 -0800

    change content of spec for initial context

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,10 +8,10 @@ describe Calculator do
       calculator = Calculator.new
 
       # 2. Exercise - perform the thing that you're testing
-      sum = calculator.add(5, 10) 
+      sum = calculator.add(1, 1) 
 
       # 3. Verification - verify that the exercise did what you expected
-      expect(sum).to eq(15)
+      expect(sum).to eq(2)
 
       # 4. Teardown - undo any conditions that shouldn't persist post-test
       # (In this test the teardown phase isn't necessary.)
